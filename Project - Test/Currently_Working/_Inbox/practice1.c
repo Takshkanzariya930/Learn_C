23eeadded27facfdcb7b392e8bb3a6a0915a76ab
Takshkanzariya930 <takshkanzariya930@gmail.com>
2024-12-18 11:55:01 +0530

    sorted some files

---
+++ Project - Test/Currently_Working/_Inbox/practice1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()  
+{ 
+	int x = 20, y = 35;
+
+	x = y++ + x++;
+	y = ++y + ++x;
+
+	printf ("%d %d", x, y);
+}
---
+++ Project - Test/Currently_Working/_Inbox/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int arr[5];
+
+	printf("Enter value : ");
+	scanf("%d",arr);
+
+	printf("\n%d",*arr);
+	printf("\n%d",arr[0]);
+}
